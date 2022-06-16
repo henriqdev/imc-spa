b844639387920c583e3e85ea2f02d9d49869f1b8
default_devMaroto <45500117+henriqdev@users.noreply.github.com>
2022-06-16 02:37:37 -0300

    iniciando chat

---
+++ src/pages/ChatBot.vue
@@ -0,0 +1,48 @@
+<template>
+  <q-page class="flex flex-center">
+    <div class="q-pa-md row justify-center">
+      <div style="width: 100%; max-width: 400px">
+        <q-chat-message
+          name="me"
+          avatar="https://cdn.quasar.dev/img/avatar1.jpg"
+          :text="['hey, how are you?']"
+          stamp="7 minutes ago"
+          sent
+          bg-color="amber-7"
+        />
+        <q-chat-message
+          name="Jane"
+          avatar="https://cdn.quasar.dev/img/avatar5.jpg"
+          :text="['doing fine, how r you?']"
+          stamp="4 minutes ago"
+          text-color="white"
+          bg-color="primary"
+        />
+      </div>
+      <q-input
+        rounded
+        outlined
+        v-model="message"
+      />
+    </div>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent, ref } from 'vue'
+
+export default defineComponent({
+  name: 'ChatBot',
+  setup () {
+    return {
+      message: ref(''),
+      data: ref([])
+    }
+  },
+  methods: {
+    async getMessageBot () {
+
+    }
+  }
+})
+</script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,10 +1,20 @@
 <template>
-  <q-page class="flex flex-center">
-    <img
-      alt="Quasar logo"
-      src="~assets/quasar-logo-vertical.svg"
-      style="width: 200px; height: 200px"
-    >
+  <q-page class="row flex-center">
+    <q-card bordered class="q-pa-md my-card col-10 bg-green-2">
+      <q-card-section>
+        <div class="text-h6">Converse com Ana</div>
+        <div class="text-subtitle2">Educadora alimentar virtual</div>
+      </q-card-section>
+
+      <q-card-section class="q-pt-none">
+        <q-avatar
+          size="100px"
+          font-size="52px"
+          text-color="primary"
+          icon="far fa-comments"
+        />
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
